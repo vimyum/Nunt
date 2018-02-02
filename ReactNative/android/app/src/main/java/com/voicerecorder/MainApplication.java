e011df2c231c3e1629e8799da9b21fa56233662d
Takahiro <takahiro@TakahironoMacBook-Pro.local>
2018-02-02 10:39:55 +0900

    upgrade react-native to 0.53

--- ReactNative/android/app/src/main/java/com/voicerecorder/MainApplication.java
+++ ReactNative/android/app/src/main/java/com/voicerecorder/MainApplication.java
@@ -3,9 +3,9 @@ package com.voicerecorder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,9 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNDeviceInfo(),
-            new ReactMaterialKitPackage(),
             new VectorIconsPackage(),
+            new ReactMaterialKitPackage(),
+            new RNDeviceInfo(),
             new ReactNativeAudioPackage()
       );
     }
